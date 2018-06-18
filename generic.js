180ca9b92b24304a6e0d6752d12005611887887c
Vitche Research Team Developer <developer@vitche.com>
2018-06-18 15:47:52 +0000

    1. "browser-request" reference added.

--- generic.js
+++ generic.js
@@ -1,23 +1,28 @@
-var request = require('request');
+var request;
+if ('browser' === process.env.RUNTIME) {
+	request = require('browser-request');
+} else {
+	request = require('request');
+}
 module.exports = {
-    _uri: undefined,
-    initialize: function (uri) {
-        this._uri = uri;
-    },
-    execute: function (method, entityName, methodName, arguments, callback) {
-        var uri = this._uri + '/' + entityName + '/' + methodName;
-        var handler = function (error, response, body) {
-            body = JSON.parse(body);
-            callback(body);
-        };
-        if ('POST' == method) {
-            request.post(uri, {
-                form: arguments
-            }, handler);
-        } else if ('GET' == method) {
-            request.get(uri, {
-                qs: arguments
-            }, handler);
-        }
-    }
+	_uri: undefined,
+	initialize: function (uri) {
+		this._uri = uri;
+	},
+	execute: function (method, entityName, methodName, arguments, callback) {
+		var uri = this._uri + '/' + entityName + '/' + methodName;
+		var handler = function (error, response, body) {
+			body = JSON.parse(body);
+			callback(body);
+		};
+		if ('POST' == method) {
+			request.post(uri, {
+				form: arguments
+			}, handler);
+		} else if ('GET' == method) {
+			request.get(uri, {
+				qs: arguments
+			}, handler);
+		}
+	}
 };
